6000296357673972722a15b5bec07d3a05364155
husheng <husheng@baozou.com>
2015-12-09 11:31:29 +0800

    add cmd -t -n

--- src/es-cleaner/main.go
+++ src/es-cleaner/main.go
@@ -5,42 +5,47 @@ import (
 	"github.com/jasonlvhit/gocron"
 	"github.com/mattbaird/elastigo/lib"
 	"time"
+	"flag"
 )
 
 const (
-	//需要保留的天数
-	remainNumber = 1
 	//日期模板
 	layout = "2006.01.02"
 	//index库的前半段
-	indexname = "logstash-momoapi-"
+	indexName = "logstash-momoapi-"
 )
 
-//创建es链接
-var esconn = elastigo.NewConn()
+var(
+    //创建es连接
+    esConn = elastigo.NewConn()
+    //需要保留的天数，默认10
+    remainNumber = flag.Int("n",10,"Remain Number")
+    //开始删除的时间
+	startDay = flag.String("t", "", "Start Deleting Date")
+)
 
 func main() {
+	//控制台接收参数
+    flag.Parse()
 	//删除参数日期到十天前的所有日志
-
-	startDay = flag.String("s", "10", "days before")
-
-	// deleteOldIndex(startDay)
-
+    if *startDay!=""{
+    	deletePreviousIndex()
+    }
 	//计划任务
 	gocron.Every(1).Day().At("02:00").Do(autoDelete)
 	gocron.Start()
 }
 
 //删除10天前的所有日志
-func deleteoldindex(t string) {
-	expiredate, err := time.Parse(layout, t)
+func deletePreviousIndex() {
+	expiredate, err := time.Parse(layout, *startDay)
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		limitdate := time.Now().AddDate(0, 0, (1 - remainNumber))
+		limitdate := time.Now().AddDate(0, 0, (1 - *remainNumber))
 		expireduration := (int)(expiredate.Sub(limitdate).Minutes() / 60 / 24)
 		for expireduration < 0 {
-			deleteIndex(limitdate.AddDate(0, 0, expireduration).Format(layout))
+			deleteIndex(indexName+limitdate.AddDate(0, 0, expireduration).Format(layout))
 			expireduration++
 		}
 	}
@@ -48,12 +53,12 @@ func deleteoldindex(t string) {
 
 //计划任务，自动删除
 func autoDelete() {
-	deleteIndex(time.Now().AddDate(0, 0, -10).Format(layout))
+	deleteIndex(indexName+time.Now().AddDate(0, 0, 1 - *remainNumber).Format(layout))
 }
 
 //删除某天日志
 func deleteIndex(index string) {
-	_, err := esconn.DeleteIndex(indexname + index)
+	_, err := esConn.DeleteIndex(indexName + index)
 
 	if err != nil {
 		fmt.Printf("del index error: %v \n", err)
